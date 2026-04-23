7299446c5019aaa60a90643b6680795856771649
ChunAn Wu <an.wu@canonical.com>
2026-04-23 13:49:13 +0800

    Create DRM test unit
    Signed-off-by: ChunAn Wu <an.wu@canonical.com>

---
+++ manage.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from plainbox.provider_manager import setup, N_
+
+setup(
+    name="checkbox-provider-kprovider",
+    namespace="com.canonical.certification",
+    version="1.0",
+    description=N_("kprovider"),
+    gettext_domain="checkbox-provider-kprovider",
+)
